7d51bc978edae51674182f365bd69ac73f549fea
Noah Velasco <noahvelasco299@gmail.com>
2023-09-05 14:01:06 -0500

    connect front end to backend successfully

--- backend/src/main/java/com/oksee/oksee/config/DBLoader.java
+++ backend/src/main/java/com/oksee/oksee/config/DBLoader.java
@@ -4,7 +4,7 @@ import java.io.InputStream;
 import java.util.List;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
+// import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.type.TypeReference;
--- backend/src/main/java/com/oksee/oksee/model/Establishment.java
+++ backend/src/main/java/com/oksee/oksee/model/Establishment.java
@@ -1,20 +1,19 @@
 package com.oksee.oksee.model;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
 
 public record Establishment(
-                @Id Integer id,
-                String category,
-                String associatedFilter,
-                String title,
-                String serviceOptions,
-                String rating,
-                String hours,
-                String address,
-                String thumbnail,
-                double latitude,
-                double longitude
+        @Id Integer id,
+        String category,
+        String associatedFilter,
+        String title,
+        String serviceOptions,
+        String rating,
+        String hours,
+        String address,
+        String thumbnail,
+        double latitude,
+        double longitude
 
 ) {
 
