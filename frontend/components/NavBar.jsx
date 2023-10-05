4b935afc89080f48727eedf222b0b0ead827faa9
Noah Velasco <noahvelasco299@gmail.com>
2023-10-05 15:55:43 -0500

    update background

--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -2,9 +2,8 @@ import SearchBar from "./SearchBar"
 
 const NavBar = () => {
   return (
-      <div className='grid justify-items-center'>
+      <div className=" grid justify-items-center">
           <h1 className='font-sans text-3xl text-[#C0A080] pt-3 font-normal md:font-bold '>Establishments</h1>
-        
           <SearchBar />
 
 
